3312f257c58714eb1a353a8cdd3ad150d0d1882f
Lolomgrofl <luka.vuksanovic@hey.com>
2024-06-11 19:10:13 +0200

    Delete unused user-related code and files

--- app/routers/api_router.py
+++ app/routers/api_router.py
@@ -1,8 +1,5 @@
 from fastapi import APIRouter
 
-from app.routers import user
 from app.settings import settings
 
 api_router = APIRouter(prefix=settings.API_V1_STR)
-
-api_router.include_router(user.router)
